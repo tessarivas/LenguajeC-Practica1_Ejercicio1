262cabf7bdf336d0d5f1f3081fb0b7f7ba52f35f
tessarivas <rivas.gomez.teresa@gmail.com>
2023-08-09 16:03:50 -0700

    Agregue codigo

--- Practica2.Pruebas.cpp
+++ Practica2.Pruebas.cpp
@@ -1,1 +1,15 @@
 //Prueba de Clase
+
+#include<stdio.h>
+int main() {
+    int limite;
+    int resultado=0;
+
+    printf("Ingrese un numero entero positivo:");
+    scanf("%d", &limite);
+    for(int i=1; i<=limite; i++){
+        resultado +=i;
+    }
+    printf("La Suma de los primeros %d numeros es: %d\n", limite, resultado);
+    return 0;
+}
